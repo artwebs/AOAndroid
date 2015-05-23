02c9a1f0883166a9841b6af3a28a072c90dac894
刘洪彬 <artwebs@126.com>
2015-05-23 19:36:47 +0800

    修正错误类信息

--- src/cn/artobj/aoandroid/MyActivity.java
+++ src/cn/artobj/aoandroid/MyActivity.java
@@ -2,6 +2,7 @@ package cn.artobj.aoandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import cn.artobj.android.application.AppApplication;
 
 public class MyActivity extends Activity {
     /**
