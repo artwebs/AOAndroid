65e6023feccf0787afb055fd6f5aa88ca041a604
刘洪彬 <artwebs@126.com>
2015-05-25 18:21:17 +0800

    优化json目录结构

--- src/cn/artobj/aoandroid/MyActivity.java
+++ src/cn/artobj/aoandroid/MyActivity.java
@@ -2,8 +2,6 @@ package cn.artobj.aoandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import cn.artobj.android.application.AppApplication;
-
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
